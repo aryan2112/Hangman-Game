1a36391ca80a8cac9e4b54f391a7451df2cda88d
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-01-20 13:31:47 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -8,8 +8,6 @@ print("Hello " + name + "! Best of Luck!")
 time.sleep(2)
 print("The game is about to start!\n Let's play Hangman!")
 time.sleep(3)
-
-
 # The parameters we require to execute the game:
 def main():
     global count

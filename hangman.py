5d5370f479ffcaf0c5ac6f9254f7914f98b18707
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-02-08 12:36:58 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,7 +1,6 @@
 import random
 import time
 #hangman
-# Initial Steps to invite in the game:
 print("\nWelcome to Hangman game by DataFlair\n")
 name = input("Enter your name: ")
 print("Hello " + name + "! Best of Luck!")

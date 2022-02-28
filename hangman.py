7cdcbacd0ba5c2bc3a6bdc03eb737396516a991d
Aryan Rai <78547046+aryan2112@users.noreply.github.com>
2022-02-28 16:56:56 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,9 +1,9 @@
 import random
 import time
 
-print("\nwelcome to Hangman game by DataFlair\n")
+print("\nWelcome to Hangman game by DataFlair\n")
 name = input("Enter your name: ")
-print("Hello " + name + "! Best of Luck!")
+print("Hello " + name + "! Best of Luck! ")
 time.sleep(2)
 print("The game is about to start!\n Let's play Hangman!")
 time.sleep(3)
